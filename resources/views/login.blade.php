08ad4ce76a0cba077a1a8ce1188d57462f9d18f3
FaliqHusnan <faliqhusnan12@gmail.com>
2025-01-14 23:00:47 +0700

    cobain1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,6 +19,36 @@
             <div class="inner-banner h-full w-full flex items-center justify-center py-10 backdrop-blur-sm bg-black/50 ">
                 <img src="img/banner.jpeg" alt="" class="h-full object-contain">
             </div>
+            <div>
+                @if (Route::has('login'))
+                            <nav class="-mx-3 flex flex-1 justify-end">
+                                @auth
+                                    <a
+                                        href="{{ url('/dashboard') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Dashboard
+                                    </a>
+                                @else
+                                    <a
+                                        href="{{ route('login') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Log in
+                                    </a>
+
+                                    @if (Route::has('register'))
+                                        <a
+                                            href="{{ route('register') }}"
+                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                            Register
+                                        </a>
+                                    @endif
+                                @endauth
+                            </nav>
+                        @endif
+            </div>
         </div>
         <div id="trending" class="h-screen bg-red-500">
 
