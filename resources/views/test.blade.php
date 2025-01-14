d5cf34af601047ba9abaa7757e956c6574d21148
CesnaSusanto <revosusanto@gmail.com>
2025-01-15 01:44:48 +0700

    add login and register button

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -10,45 +10,41 @@
 </head>
 <body class="bg-[#131B21]">
     <nav class="bg-[#333A3F] w-full h-[75px] border-b-[3px] px-8 border-b-[#007DE4] shadow-lg shadow-[#007de49f] sticky z-10 top-0">
-        <div class="h-full w-full">
+        <div class="h-full w-full flex flex-row justify-between items-center">
             <img src="img/logo.png" alt="logo" srcset="" class="h-full object-contain">
+            <div class="flex gap-4 items-center text-white uppercase">
+                    @if (Route::has('login'))
+                                    @auth
+                                        <a
+                                            href="{{ url('/dashboard') }}"                                        >
+                                            Dashboard
+                                        </a>
+                                    @else
+                                        <a
+                                            href="{{ route('login') }}"                                        >
+                                            Login
+                                        </a>
+
+                                        @if (Route::has('register'))
+                                            <a
+                                                href="{{ route('register') }}"
+                                                class=" px-[10px] py-[7px] bg-white text-[#333A3F] rounded-md font-semibold"
+                                            >
+                                                Register
+                                            </a>
+                                        @endif
+                                    @endauth
+                            @endif
+                    </div>
         </div>
     </nav>
     <div id="dashboard" class="relative">
         <div id="banner" class="w-full h-[600px] bg-cover" style="background-image: url('img/banner.jpeg');">
             <div class="inner-banner h-full w-full flex items-center justify-center py-10 backdrop-blur-sm bg-black/50 ">
                 <img src="img/banner.jpeg" alt="" class="h-full object-contain">
+                
             </div>
-            <div>
-                @if (Route::has('login'))
-                            <nav class="-mx-3 flex flex-1 justify-end">
-                                @auth
-                                    <a
-                                        href="{{ url('/dashboard') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Dashboard
-                                    </a>
-                                @else
-                                    <a
-                                        href="{{ route('login') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Log in
-                                    </a>
-
-                                    @if (Route::has('register'))
-                                        <a
-                                            href="{{ route('register') }}"
-                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
-                                            Register
-                                        </a>
-                                    @endif
-                                @endauth
-                            </nav>
-                        @endif
-            </div>
+            
         </div>
         <div id="trending" class="h-screen bg-red-500">
 
